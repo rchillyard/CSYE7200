d6f1ee4fc1d574386c4267fc409e5708ce58dcc4
JiaqiWang <wang.jiaqi6@northeastern.edu>
2021-09-20 02:00:11 -0400

    modify the partial code and get result 15

--- assignment-helloworld/src/main/scala/edu/neu/coe/csye7200/assthw/Ingest.scala
+++ assignment-helloworld/src/main/scala/edu/neu/coe/csye7200/assthw/Ingest.scala
@@ -35,7 +35,9 @@ object Ingest extends App {
     case h :: _ => Source.fromFile(h)
   }
 
-  for (m <- ingester(source)) println(m.properties.mkString(", "))
+//  for (m <- ingester(source)) println(m.properties.mkString(", "))
+val kiwiMovies = for (m <- ingester(source); if (m.properties(20)=="New Zealand")) yield m
+  println(kiwiMovies.size)
   source.close()
 
   // Please note that an alternative to the definition of source above would be as in the following comment:
